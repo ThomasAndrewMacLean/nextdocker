7f9c52a13173a9bd35524c7fd5ce303e72655cd7
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-12-11 15:35:28 +0100

    c

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+//require('dotenv').config();
 
 module.exports = {
-    env: {
-        DB_PASSWORD: process.env.DB_PASSWORD,
-        DB_USER: process.env.DB_USER,
-        DB_DATABASE: process.env.DB_DATABASE,
-        DB_HOST: process.env.DB_HOST
-    }
+    // env: {
+    //     // DB_PASSWORD: process.env.DB_PASSWORD,
+    //     // DB_USER: process.env.DB_USER,
+    //     // DB_DATABASE: process.env.DB_DATABASE,
+    //     // DB_HOST: process.env.DB_HOST
+    // }
 };
