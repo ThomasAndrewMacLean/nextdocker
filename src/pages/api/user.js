66798bdedb1cff0d3ae6cb682fb405765fea0dcc
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-12-11 14:17:29 +0100

    testing socketPath again

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,14 +1,16 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize({
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-        socketPath: process.env.DB_SOCKETPATH
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        dialectOptions: {
+            socketPath: process.env.DB_SOCKETPATH
+        }
     }
-});
+);
 
 class User extends Model {}
 User.init(
