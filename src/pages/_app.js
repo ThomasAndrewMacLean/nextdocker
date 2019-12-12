976d8a256e1930db1124704615b503c66f5e6ab9
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-12-12 16:53:48 +0100

    test

---
+++ src/pages/_app.js
@@ -0,0 +1,26 @@
+import App from 'next/app';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'production') {
+    Sentry.init({
+        dsn: 'https://0d5a6b06cd244091820d9ba99846bbbb@sentry.io/1849962'
+    });
+}
+
+export default class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props;
+
+        // Workaround for https://github.com/zeit/next.js/issues/8592
+        const { err } = this.props;
+        const modifiedPageProps = { ...pageProps, err };
+
+        return (
+            <ThemeProvider theme={theme}>
+                <Component {...modifiedPageProps} />
+            </ThemeProvider>
+        );
+    }
+}
